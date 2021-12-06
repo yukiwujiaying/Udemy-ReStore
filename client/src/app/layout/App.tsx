fc743e1d59cbab5c247b52904b9eae7bd089a65e
ywu11 <yuki.wu@kpmg.co.uk>
2021-12-06 22:00:42 +0000

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,6 +4,11 @@ import Header from "./Header";
 import createTheme from '@mui/material/styles/createTheme';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import { useState } from "react";
+import { Route, Routes } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
   const [darkMode, setDarkmode] = useState(false);
@@ -26,8 +31,15 @@ function App() {
      <CssBaseline />
      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
      <Container>
-       <Catalog/>        
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/catalog' element={<Catalog/>}/>
+        <Route path='/catalog/:id' element={<ProductDetails/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/contact' element={<ContactPage/>}/>
+       </Routes>
      </Container>
+
     </ThemeProvider> 
   
   );
---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,61 @@
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { grid } from "@mui/system";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Product } from "../../app/layout/models/product";
+
+export default function ProductDetails() {
+    let { id } = useParams();
+    const [product, setProduct]= useState<Product | null>(null);
+    const [loading, setloading]= useState(true);
+
+    useEffect(()=>{
+        axios.get(`http://localhost:5000/api/products/${id}`)
+             .then(response => setProduct(response.data))
+             .catch(error=> console.log(error))
+             .finally(()=>setloading(false));
+    },[id])
+
+    if (loading) return <h3>Loading...</h3>
+
+    if(!product) return <h3>Product not found</h3>
+    return(
+        <Grid container spacing={6}>
+            <Grid item xs={6}>
+                <img src={product.pictureUrl} alt={product.name} style={{width:'100%'}}/>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant='h3'>{product.name}</Typography>
+                <Divider sx={{mb:2}}/>
+                <Typography variant='h4' color='secondary'>£{(product.price/100).toFixed(2)}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.description}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Type</TableCell>
+                                <TableCell>{product.type}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{product.brand}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity in stock</TableCell>
+                                <TableCell>{product.quantityInStock}</TableCell>
+                            </TableRow>                         
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
+    )
+}
